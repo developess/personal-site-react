b5f3ab011cb84a67b84ff116415f606684f90485
Jessie Beech <jessie@jessiebeech.com>
2019-01-02 10:51:00 +0000

    added more content to skillsContent

--- src/components/skillsContent.js
+++ src/components/skillsContent.js
@@ -1,29 +1,65 @@
 const skills = [
   {
-    id: "0",
-    name: "JavaScript",
-    class: "javascript",
+    id: '0',
+    name: 'JavaScript',
+    class: 'javascript',
     text:
-      "I started learning JavaScript over a year ago when I was first teaching myself to code."
+      'I started learning JavaScript in June 2017 when I was first teaching myself to code. Its opened up to me the wonderful world of web development and helped me discover Founders and Coders, a free coding bootcamp where I learned many aspects of full stack Javascript and its far-reaching ecosystem.',
   },
-  { id: "1", name: "Node", class: "node", text: "" },
-  { id: "2", name: "PostgreSQL", class: "database", text: "" },
-  { id: "3", name: "React", class: "frontend", text: "" },
-  { id: "4", name: "Express", class: "node", text: "" },
-  { id: "5", name: "Handlebars", class: "frontend", text: "" },
-  { id: "6", name: "CSS", class: "css", text: "" },
-  { id: "7", name: "Sass", class: "css", text: "" },
-  { id: "8", name: "APIs", class: "node", text: "" },
-  { id: "9", name: "Git", class: "git", text: "" },
-  { id: "10", name: "Agile", class: "methodology", text: "I " },
   {
-    id: "11",
-    name: "TDD",
-    class: "methodology",
-    title: "Test Driven Development",
+    id: '1',
+    name: 'Node',
+    class: 'node',
     text:
-      "I am a firm believer in the importance of Test Driven Development and writing modular, testable code. I have experience with unit testing and integration testing on both frontend and backend. I am familar with Travis (CI) and Code Coverage tools but believe testing for edge cases is key to a resilient application."
-  }
+      "Learning Node was my first taste of backend development, an area which captured my interest as something varied, challenging and new. I've since experimented with backend technology in python and elixir, but Node with Express remains the go to server side setup in my toolkit.",
+  },
+  {
+    id: '2',
+    name: 'PostgreSQL',
+    class: 'database',
+    text:
+      "I learnt about relational databases, SQL and database design during Founders and Coders and we frequently used SQL databases for our projects. I thought it such an interesting aspect of backend development that and I opted to be a mentor for database week in the following cohort. While there are many interesting new alternatives to database storage emerging all the time, I don't suppose the SQL database will go out of fashion anytime soon.",
+  },
+  {
+    id: '3',
+    name: 'React',
+    class: 'frontend',
+    text:
+      "I learnt react during the latter part of Founders and Coders and have hardly stopped using it since! Joining InFact Coop after graduating meant building several interesting apps with React, Redux, Gatsby and Styled Components. It's awesome to learn a technology that represents such a shift in frontend devleopment, and its existence motivates me to think critically about the tools I use and how they could be adapted and reworked into a totally different development paradigm.",
+  },
+  { id: '4', name: 'Express', class: 'node', text: '' },
+  {
+    id: '5',
+    name: 'Handlebars',
+    class: 'frontend',
+    text:
+      'I think templating engines still have an important place where UI responsiveness is not a priority and React would be overkill. I found Handlebars to be intuitive and powerful and I can see why its such a useful tool in a developers toolbox.',
+  },
+  {
+    id: '6',
+    name: 'CSS/Sass',
+    class: 'css',
+    text:
+      "I really enjoyed playing with CSS while learning to code and its stood me in great stead for Founders and Coders and my career beyond. I made name for myself in my cohort of Founders and Coders with an eclectic knowledge of lesser-known CSS properties and a knack for animations, transitions and transforms. I've lately adapted these skills for use with Styled Components, Tachyons and other interesting CSS technologies.",
+  },
+  {
+    id: '7',
+    name: 'Styled Components',
+    class: 'css',
+    text:
+      'I really enjoy using styled components and would find it strange to go back to conventional CSS. ',
+  },
+  { id: '8', name: 'APIs', class: 'node', text: '' },
+  { id: '9', name: 'Git', class: 'git', text: '' },
+  { id: '10', name: 'Agile', class: 'methodology', text: 'I ' },
+  {
+    id: '11',
+    name: 'TDD',
+    class: 'methodology',
+    title: 'Test Driven Development',
+    text:
+      'I am a firm believer in the importance of Test Driven Development and writing modular, testable code. I have experience with unit testing and integration testing on both frontend and backend. I am familar with Travis (CI) and Code Coverage tools but believe testing for edge cases is key to a resilient application.',
+  },
 ];
 
 export default skills;
